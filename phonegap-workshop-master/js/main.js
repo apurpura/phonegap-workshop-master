7f834c0ff15a34861dede52e44388bea39ccdcfe
Andrew <apurpura@zagmail.gonzaga.edu>
2013-12-18 16:57:08 -0800

    changed to view

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -13,17 +13,23 @@ var app = {
         });
     },
 
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }},
+
+
+    renderHomeView: function() {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));},
 
     initialize: function() {
         var self = this;
         this.store = new WebSqlStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
@@ -31,3 +37,10 @@ var app = {
 };
 
 app.initialize();
+
+function showAlert (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }}
