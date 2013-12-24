3481bd407355871b807c6991018f54e25b028d94
Andrew <apurpura@zagmail.gonzaga.edu>
2013-12-23 23:07:47 -0800

    project id added

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -68,7 +68,7 @@ var app = {
         }},
 
     addListener: function() {
-        var pushNotification = window.plugins.pushNotification;pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"824841663931","ecb":"app.onNotificationGCM"});
+        var pushNotification = window.plugins.pushNotification;pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"pickup-game-662495","ecb":"app.onNotificationGCM"});
     },
 
     // result contains any message sent from the plugin
