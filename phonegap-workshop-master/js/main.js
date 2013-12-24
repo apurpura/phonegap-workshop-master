70e0f40ce8949331998ec48a3eef78882f87b38a
Andrew <apurpura@zagmail.gonzaga.edu>
2013-12-24 00:03:24 -0800

    t

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -67,48 +67,9 @@ var app = {
             });
         }},
 
-    addListener: function() {
-        var pushNotification = window.plugins.pushNotification;
-        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"pickup-game-662495","ecb":"app.onNotificationGCM"});
-    },
-
-    // result contains any message sent from the plugin
-    successHandler: function(result) {
-        alert('Callback Success! Result = '+result)
-    },
-
-    errorHandler:function(error) {
-        alert(error);
-    },
-
-    onNotificationGCM: function(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    console.log("Regid " + e.regid);
-                    alert('registration id = '+e.regid);
-                }
-                break;
-            case 'message':
-                     // this is the actual push notification. its format depends on the data model from the push server
-                           alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-                break;
-            case 'error':
-                alert('GCM error = '+e.msg);
-                break;
-            default:
-                alert('An unknown GCM event has occurred');
-                break;
-        }
-    },
-
-
     initialize: function() {
         var self = this;
         this.detailsURL = /^#employees\/(\d{1,})/;
-        this.addListener();
         this.registerEvents();
         this.store = new MemoryStore(function() {
             self.route();
@@ -117,4 +78,115 @@ var app = {
 
 };
 
+document.addEventListener('deviceready', onDeviceReady, true);
 app.initialize();
+
+
+var pushNotification = window.plugins.pushNotification;
+
+function onDeviceReady() {
+    if ( device.platform == 'android' || device.platform == 'Android' )
+    {
+        pushNotification.register(
+            successHandler,
+            errorHandler, {
+                "senderID":"pickup-game-662495 ",
+                "ecb":"onNotificationGCM"
+            });
+    }
+    else
+    {
+        pushNotification.register(
+            tokenHandler,
+            errorHandler, {
+                "badge":"true",
+                "sound":"true",
+                "alert":"true",
+                "ecb":"onNotificationAPN"
+            });
+    }
+}
+
+// result contains any message sent from the plugin call
+function successHandler (result) {
+    alert('result = ' + result);
+}
+
+// result contains any error description text returned from the plugin call
+function errorHandler (error) {
+    alert('error = ' + error);
+}
+
+// iOS
+function onNotificationAPN (event) {
+    if ( event.alert )
+    {
+        navigator.notification.alert(event.alert);
+    }
+
+    if ( event.sound )
+    {
+        var snd = new Media(event.sound);
+        snd.play();
+    }
+
+    if ( event.badge )
+    {
+        pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+    }
+}
+
+// Android
+function onNotificationGCM(e) {
+    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+    switch( e.event )
+    {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            {
+                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+                // Your GCM push server needs to know the regID before it can push to this device
+                // here is where you might want to send it the regID for later use.
+                console.log("regID = " + e.regid);
+            }
+            break;
+
+        case 'message':
+            // if this flag is set, this notification happened while we were in the foreground.
+            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+            if ( e.foreground )
+            {
+                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+                // if the notification contains a soundname, play it.
+                var my_media = new Media("/android_asset/www/"+e.soundname);
+                my_media.play();
+            }
+            else
+            {  // otherwise we were launched because the user touched a notification in the notification tray.
+                if ( e.coldstart )
+                {
+                    $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                }
+                else
+                {
+                    $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                }
+            }
+
+            $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+            $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+            break;
+
+        case 'error':
+            $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+            break;
+
+        default:
+            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+            break;
+    }
+}
+
+
